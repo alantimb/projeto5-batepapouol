67055b5a730af9b9ffc9ab9efc1ada7bfa987fd8
Alan Timbó <alancorreiatimbo@gmail.com>
2022-08-27 23:41:51 -0300

    projeto5.1 - layout inicial aprimorado e início do js

---
+++ script.js
@@ -0,0 +1,7 @@
+// função para perguntar o nome do usuário ao entrar
+function entrarNaSala() {
+    let nomeDoUsuario = prompt("Digite aqui como quer ser chamado(a): ");
+
+    console.log(nomeDoUsuario);
+}
+
